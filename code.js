f3c4c66fb0bdb5380a9c18d926b5c843638f62cd
Anshuman Sinha <sinhaanshuman27@gmail.com>
2021-06-11 07:34:06 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,7 @@
+//Use this code inside the code section of the product configuration on the Bolt Cloud
+
+setChartLibrary('google-chart');
+setChartTitle('Your Graph Title');
+setChartType('predictionGraph'); 
+setAxisName('X-axis Name','Y-axis Name');
+plotChart('time_stamp','light_data');
